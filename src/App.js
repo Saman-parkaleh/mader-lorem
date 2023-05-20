343910099747b8f161ebc5d2a8f9d28e46674815
saman <ssaman.parkaleh76@gmail.com>
2023-05-20 15:28:21 +0430

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import data from "./Data/Data";
 
 function App() {
+  const [cont, setCont] = useState(0);
+  const [text, setText] = useState([]);
+
+  const formonsubmit = (e) => {
+    e.preventDefault();
+    let mount = parseInt(cont);
+    console.log(typeof mount);
+    if (cont < 0) mount = 0;
+    if (cont > 5) mount = 5 ;
+    setText(data.slice(0, mount));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contaner">
+      <div className="heder">
+        <h1> maker lorem</h1>
+      </div>
+      <form onSubmit={formonsubmit}>
+        <label className="label">number of paragraphs : </label>
+        <input
+          className="input"
+          type={"number"}
+          value={cont}
+          name={"total"}
+          onChange={(e) => setCont(e.target.value)}
+        />
+        <button className="btn" type="submit">
+          maker
+        </button>
+      </form>
+      <article>
+        {text.map((item, index) => {
+          return (
+            <p className=" text" key={index}>
+              {item}
+            </p>
+          );
+        })}
+      </article>
     </div>
   );
 }
-
 export default App;
